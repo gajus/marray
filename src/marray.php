9008743defd63c2990ff9f35eefe79a37df9fe49
Gajus Kuizinas <g.kuizinas@anuary.com>
2014-02-16 18:33:21 +0000

    Functional.

--- src/marray.php
+++ src/marray.php
@@ -1,11 +1,11 @@
 <?php
 namespace Gajus\Marray;
 
-static private function intersect_inverse (array $whitelist, array $input) {
+function intersect_inverse (array $whitelist, array $input) {
     return array_intersect_key($input, array_flip($whitelist));
 }
 
-static private function intersect_recursive (array $arr1, array $arr2) {
+function intersect_recursive (array $arr1, array $arr2) {
     $return = [];
     
     $common_keys = array_intersect(array_keys($arr1), array_keys($arr2));
@@ -27,13 +27,13 @@ static private function intersect_recursive (array $arr1, array $arr2) {
         
         array_splice($arguments, 0, 2, [$return]);
     
-        $return = call_user_func_array(['arrayIntersectRecursive', self], $arguments);
+        $return = call_user_func_array('Gajus\Marray\arrayIntersectRecursive', $arguments);
     }
     
     return $return;
 }
 
-static private function diff_key_recursive (array $arr1, array $arr2) {
+function diff_key_recursive (array $arr1, array $arr2) {
     $diff = array_diff_key($arr1, $arr2);
     $intersect = array_intersect_key($arr1, $arr2);
     
